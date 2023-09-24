9c35ec8a0c945fc2b854908a67e51c3c7aaad2a7
Ondra58 <97514734+Ondra58@users.noreply.github.com>
2023-09-24 18:24:32 +0200

    Add files via upload

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(391, 282);
+            this.button1.Location = new System.Drawing.Point(113, 149);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -45,16 +46,25 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(188, 225);
+            this.listBox1.Location = new System.Drawing.Point(292, 107);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.Size = new System.Drawing.Size(161, 95);
             this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(459, 107);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(161, 95);
+            this.listBox2.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -67,6 +77,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Net.Sockets;
 
 namespace WindowsFormsApp1
 {
@@ -17,11 +18,46 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
-        int vek;
-        public int Vek()
+        public void Vek(string[] pole, ref bool prosinec, out int vek, ref string mesic2)
         {
-            return vek;
-        }       
+            string jmeno = pole[0];
+            string rodnecislo = pole[2];
+            int rok = 0;           
+            if ((((int)rodnecislo[2] - 48 == 1 && (int)rodnecislo[3] - 48 == 2) || ((int)rodnecislo[2] - 48 == 6 && (int)rodnecislo[3] - 48 == 2)) && prosinec == false)
+            {
+                MessageBox.Show("První člověk, jenž je narozen v prosinci: " + jmeno);
+                prosinec = true;
+            }
+            if (Convert.ToInt32(rodnecislo.Substring(0, 2)) > 23)
+            {
+                rok = Convert.ToInt32(rodnecislo.Substring(0, 2)) + 1900;
+            }
+            else
+            {
+                rok = Convert.ToInt32(rodnecislo.Substring(0, 2)) + 2000;
+            }           
+            int mesic = Convert.ToInt32(rodnecislo.Substring(2, 2));
+            int den = Convert.ToInt32(rodnecislo.Substring(4, 2));
+            DateTime datumanrozeni = new DateTime(rok, mesic, den);
+            TimeSpan cc = new TimeSpan();
+            cc = (DateTime.Now - datumanrozeni);
+            vek = (int)cc.TotalDays / 365;
+            switch(mesic)
+            {
+                case 1: { mesic2 = "leden"; break; }
+                case 2: { mesic2 = "únor"; break; }
+                case 3: { mesic2 = "březen"; break; }
+                case 4: { mesic2 = "duben"; break; }
+                case 5: { mesic2 = "květen"; break; }
+                case 6: { mesic2 = "červen"; break; }
+                case 7: { mesic2 = "červenec"; break; }
+                case 8: { mesic2 = "srpen"; break; }
+                case 9: { mesic2 = "září"; break; }
+                case 10: { mesic2 = "říjen"; break; }
+                case 11: { mesic2 = "listopad"; break; }
+                case 12: { mesic2 = "prosinec"; break; }
+            }
+        }  
         private void button1_Click(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
@@ -29,6 +65,9 @@ namespace WindowsFormsApp1
             double prumer = 0;
             int pocet = 0;
             bool prosinec = false;
+            int vek;
+            int[] poleveku = new int[3];
+            string mesic2 = "";
             while (!ctenar.EndOfStream)
             {
                 string[] pole = ctenar.ReadLine().Split(';');
@@ -36,14 +75,10 @@ namespace WindowsFormsApp1
                 string jmeno = pole[0];
                 double znamka = double.Parse(pole[1]);
                 string rodnecislo = pole[2];
+                Vek(pole, ref prosinec, out vek, ref mesic2);
+                poleveku[pocet] = vek;                      
                 prumer += znamka;
-                pocet++;
-                //MessageBox.Show(rodnecislo[2].ToString() + rodnecislo[3].ToString());
-                if ((((int)rodnecislo[2] == 1 && (int)rodnecislo[3] == 2) || ((int)rodnecislo[2] == 6 && (int)rodnecislo[3] == 2)) && prosinec == false)
-                {
-                    MessageBox.Show("První člověk, jenž je narozen v prosinci: " + jmeno);
-                    prosinec = true;
-                }
+                pocet++;               
             }
             ctenar.Close();
             if(pocet != 0)
@@ -51,8 +86,35 @@ namespace WindowsFormsApp1
                 prumer /= pocet;
             }
             StreamWriter zapisovak = new StreamWriter("rodna_cisla.txt", true);
-            zapisovak.WriteLine("Průměr všech známek: " + prumer);
+            zapisovak.WriteLine("\nPrůměr všech známek: " + prumer);
+            int i = 0;
+            foreach (string zaznam in listBox1.Items)
+            {
+                string novyzaznam = zaznam + ";" + poleveku[i];
+                listBox2.Items.Add(novyzaznam);
+                i++;
+            }
             zapisovak.Close();
+
+            SaveFileDialog ulozeni = new SaveFileDialog();
+            ulozeni.InitialDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+            string nazev = "";
+            if (ulozeni.ShowDialog() == DialogResult.OK)
+            {
+                nazev = ulozeni.FileName;
+            }
+            StreamWriter zapisovak2 = new StreamWriter(nazev, true);
+            foreach (string zaznam in listBox1.Items)
+            {
+                string[] pole = zaznam.Split(';');
+                string jmeno = pole[0];
+                double znamka = double.Parse(pole[1]);
+                Vek(pole, ref prosinec, out vek, ref mesic2);
+                if (znamka < 3)
+                {
+                    zapisovak2.WriteLine(jmeno+ ";" + znamka + ";" + mesic2);
+                }
+            }
         }
     }
 }
